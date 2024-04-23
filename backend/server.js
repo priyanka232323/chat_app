4df790eed26af4dffdc1ca5e651e35148a34187a
Priyanka <rwtpriyanka2302@gmail.com>
2024-04-24 05:02:14 +0530

    4th part completed

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,11 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
+// created in socket
+// const app = express();
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 
@@ -29,7 +31,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT,() =>{
+server.listen(PORT,() =>{
 	connectToMongoDB();
  console.log(`server is Running on port ${PORT}`)
 });
